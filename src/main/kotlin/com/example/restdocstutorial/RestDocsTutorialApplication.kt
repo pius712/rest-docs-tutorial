101cd3ed5f68419bb5167761fd759786b1a96348
pius712 <aksfbsgnlfls@gmail.com>
2023-10-29 23:17:34 +0900

    first commit

---
+++ src/main/kotlin/com/example/restdocstutorial/RestDocsTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restdocstutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestDocsTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestDocsTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/restdocstutorial/RestDocsTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restdocstutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestDocsTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
